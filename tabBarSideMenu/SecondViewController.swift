c04a037b88c3b3d2394129a57688361c086064d3
shahrukh shikalgar <shaharukhs@gmail.com>
2017-06-22 18:15:42 +0530

    initial commit

--- tabBarSideMenu/SecondViewController.swift
+++ tabBarSideMenu/SecondViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import InteractiveSideMenu
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, SideMenuItemContent{
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		self.tabBarController?.tabBar.isHidden = false
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
@@ -20,6 +22,33 @@ class SecondViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	/*
+	Show menu on click if connected tab bar controller adopts proper protocol.
+	*/
+	@IBAction func openMenu(_ sender: UIButton) {
+		
+		
+		if let menuItemViewController = self.tabBarController as? SideMenuItemContent {
+			menuItemViewController.showSideMenu()
+		}
+		
+//		if let navigationViewController = self.navigationController as? SideMenuItemContent {
+//			navigationViewController.showSideMenu()
+//		}
+		
+	}
+	
+	
+	@IBAction func subView1Called(_ sender: Any) {
+
+		let vc = self.storyboard?.instantiateViewController(withIdentifier: "subView1") as! SubView1VC
+		self.navigationController?.pushViewController(vc, animated: true)
+	}
+
+	@IBAction func subView2Called(_ sender: Any) {
+		let vc = self.storyboard?.instantiateViewController(withIdentifier: "subView2") as! SubView2VC
+		self.navigationController?.pushViewController(vc, animated: true)
+	}
 
 }
 
